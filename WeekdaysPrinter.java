240e1f7cb75e68e2c1277658e2e0615e69eb0058
alhajerih <alhajerih@hotmail.com>
2024-11-04 10:33:27 +0300

    Task done

--- WeekdaysPrinter.java
+++ WeekdaysPrinter.java
@@ -3,7 +3,13 @@ public class WeekdaysPrinter {
         String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
         for (int i = 0; i < days.length; i++) {
             // In our case here, we could use either break or continue and we will get same result
-            if (i >= 5)
+            if (days[i]=="Friday")
+                break;
+            if(days[i]=="Saturday")
+                break;
+
+            //we could also dealing it with index since we know the array
+            if(i>=5)
                 break;
             System.out.println(days[i]);
         }
